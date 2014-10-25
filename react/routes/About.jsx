d7531d76f21d799a1f74e3265c863cb7b9e9ce91
Patrick Rodriguez <patr84@gmail.com>
2014-10-24 23:10:35 -0700

    * Added css animations from https://github.com/codrops/PageTransitions/blob/master/css/animations.css * Added animation on route change

--- react/routes/About.jsx
+++ react/routes/About.jsx
@@ -6,7 +6,9 @@ var React = require('react'),
 var About = React.createClass({
     render: function () {
         return (
-            <h1>About!</h1>
+            <div>
+                <h1>About!</h1>
+            </div>
         );
     }
 });
