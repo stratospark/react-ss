1046cadfe7dd15b04cc7a356e106768a0c1806ac
Igal <igal.fl@gmail.com>
2015-01-06 17:36:15 -0800

    Readd favicon

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,7 @@ var express = require('express'),
 
 var router = express.Router();
 
-///* GET home page. */
-//router.get('/', function (req, res) {
-//    res.render('index', {title: 'Express'});
-//});
-
 router.get("*", function (req, res) {
-    if(req.url == '/favicon.ico'){
-        return res.status(404).end();
-    }
     Router.renderRoutesToString(routes, req.path, function(e, ar, s) {
             res.send(s);
     })
